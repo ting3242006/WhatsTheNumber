c65de61d9938e98e677e7c8f7f0b97448cc7f086
ting3242006 <ting3242006@hotmail.com>
2021-09-22 10:33:24 +0800

    Initial Commit

---
+++ WhatsTheNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsTheNumber
+//
+//  Created by Ting on 2021/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
